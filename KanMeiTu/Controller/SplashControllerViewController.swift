55a26be70b9a982aed10d0179e4dbdb9224a53cd
xiaolaji <281232910@qq.com>
2022-10-21 15:21:57 +0800

    111

---
+++ KanMeiTu/Controller/SplashControllerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SplashControllerViewController.swift
+//  KanMeiTu
+//
+//  Created by yi on 2022/10/21.
+//
+
+import UIKit
+
+class SplashControllerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        SceneDelegate.shared.next()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
